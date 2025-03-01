0cd0690ebaeb683c65ebd77fe0f010700649bc3c
Kasun Viranga <kasunviranga12@gmail.com>
2025-03-01 12:45:30 +0530

    feat: add Usage component for feature usage tracking

--- action/analyseYoutubeVideo.ts
+++ action/analyseYoutubeVideo.ts
@@ -1,13 +1,15 @@
 "use server";
 
+import { getVideoIdFromUrl } from "@/lib/getVideoIdFromUrl";
 import { redirect } from "next/navigation";
 
 export async function analyseYoutubeVideo(formData: FormData) {
   const url = formData.get("url")?.toString();
   if (!url) return;
 
-  //   const videoId = getVideoIdFromUrl(url);
-  const videoId = "abc"; // fake video id
+  //   const videoId = "123" //fake id
+  const videoId = getVideoIdFromUrl(url);
+  // console.log(videoId);
   if (!videoId) return;
 
   // Refirect to the new post
